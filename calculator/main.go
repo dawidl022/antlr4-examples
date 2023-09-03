114c8ad5d7d333df78d889d78d53185eedf87a97
Dawid Lachowicz <domdaw7@gmail.com>
2023-09-03 20:24:59 +0100

    Add blank lines for readability

--- calculator/main.go
+++ calculator/main.go
@@ -12,9 +12,11 @@ func main() {
 	input := antlr.NewIoStream(os.Stdin)
 	lexer := parser.NewCalculatorLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
+
 	p := parser.NewCalculatorParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
 	p.BuildParseTrees = true
+
 	tree := p.Init()
 	eval := interpreter.NewEvalVisitor()
 	eval.Visit(tree)
